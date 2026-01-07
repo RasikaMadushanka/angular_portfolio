70da86298a3d661f640f7b9162555a7b403737cc
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-01-07 12:57:19 +0530

    home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../../component/nav-bar/nav-bar.component";
+import { FooterComponent } from "../../component/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
