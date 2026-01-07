4db5561528042a85989eac8c1376f7fee7144df8
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-01-07 12:45:55 +0530

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,ContactUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {
+          path:"/",
+         component:HomeComponent
+    },
+    {
+         path:'contact',
+         component:ContactUsComponent
+    }
+];
